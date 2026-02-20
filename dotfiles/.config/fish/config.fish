5405dfd1af1d4f867edf815ae144199191e4a158
HiredNoobs <78608593+HiredNoobs@users.noreply.github.com>
2026-02-20 12:49:58 +0000

    Add ANSIBLE_HOST_KEY_CHECKING to fish config

--- dotfiles/.config/fish/config.fish
+++ dotfiles/.config/fish/config.fish
@@ -9,7 +9,8 @@ if status is-interactive
     fish_add_path ~/tools-bin/bin
 
     # Env vars (-x = export)
-    set -x SHELL $(which fish)  # For tmux
+    set -gx SHELL $(which fish)  # For tmux
+    set -gx ANSIBLE_HOST_KEY_CHECKING false
 
     # Aliases
     alias "tm" "tmux new-session \; split-window -h -p 50 \; select-pane -L \; attach"
