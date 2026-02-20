815647ad6e3c2956afdc732ef23f73b4b59fba70
HiredNoobs <78608593+HiredNoobs@users.noreply.github.com>
2026-02-20 21:58:29 +0000

    Send logs to stderr

--- bin/lib/logging.sh
+++ bin/lib/logging.sh
@@ -6,13 +6,13 @@ ERR_COLOR="\033[0;31m"
 WARN_COLOR="\033[1;33m"
 
 function log_info {
-  echo -e "${INFO_COLOR}$1${NO_COLOR}"
+  echo -e "${INFO_COLOR}$1${NO_COLOR}" >&2
 }
 
 function log_warning {
-  echo -e "${WARN_COLOR}$1${NO_COLOR}"
+  echo -e "${WARN_COLOR}$1${NO_COLOR}" >&2
 }
 
 function log_error {
-  echo -e "${ERR_COLOR}$1${NO_COLOR}"
+  echo -e "${ERR_COLOR}$1${NO_COLOR}" >&2
 }
