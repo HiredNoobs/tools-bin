ff9b93325d654f10c6ec9100d4e2594d78bf9293
HiredNoobs <78608593+HiredNoobs@users.noreply.github.com>
2026-02-20 16:33:40 +0000

    Add env file check to check_env

--- bin/lib/logging.sh
+++ bin/lib/logging.sh
@@ -3,9 +3,14 @@
 NO_COLOR="\033[0m"
 INFO_COLOR="\033[1;32m"
 ERR_COLOR="\033[0;31m"
+WARN_COLOR="\033[1;33m"
 
 function log_info {
   echo -e "${INFO_COLOR}$1${NO_COLOR}"
+}
+
+function log_warning {
+  echo -e "${WARN_COLOR}$1${NO_COLOR}"
 }
 
 function log_error {
