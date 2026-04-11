40da3e4b9ad25364e9d54450471ef7a371fcbdaa
HiredNoobs <78608593+HiredNoobs@users.noreply.github.com>
2026-04-11 13:08:54 +0100

    Change screenshot out dir

--- dotfiles/.config/rofi/scripts/screenshots_menu.sh
+++ dotfiles/.config/rofi/scripts/screenshots_menu.sh
@@ -4,8 +4,8 @@ OPTIONS="Fullscreen (instant)\nFullscreen (2s delay)\nSelect Area (instant)\nSel
 CHOICE=$(echo -e "$OPTIONS" | rofi -dmenu -p "Screenshots" -theme ~/.config/rofi/config.rasi)
 
 case $CHOICE in
-  "Fullscreen (instant)") grimblast --notify copysave screen ~/Downloads/$(date +%Y%m%d-%H%M%S).png ;;
-  "Fullscreen (2s delay)") sleep 2 && grimblast --notify copysave screen ~/Downloads/$(date +%Y%m%d-%H%M%S).png ;;
-  "Select Area (instant)") grimblast --freeze --notify copysave area ~/Downloads/$(date +%Y%m%d-%H%M%S).png ;;
-  "Select Area (2s delay)") sleep 2 && grimblast --freeze --notify copysave area ~/Downloads/$(date +%Y%m%d-%H%M%S).png ;;
+  "Fullscreen (instant)") grimblast --notify copysave screen ~/Pictures/screenshots/$(date +%Y%m%d-%H%M%S).png ;;
+  "Fullscreen (2s delay)") sleep 2 && grimblast --notify copysave screen ~/Pictures/screenshots/$(date +%Y%m%d-%H%M%S).png ;;
+  "Select Area (instant)") grimblast --freeze --notify copysave area ~/Pictures/screenshots/$(date +%Y%m%d-%H%M%S).png ;;
+  "Select Area (2s delay)") sleep 2 && grimblast --freeze --notify copysave area ~/Pictures/screenshots/$(date +%Y%m%d-%H%M%S).png ;;
 esac
