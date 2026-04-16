af225632bf62a20eb294f3925fd0595f5dbd2bfd
HiredNoobs <78608593+HiredNoobs@users.noreply.github.com>
2026-04-17 00:05:35 +0100

    Add basic wallpaper selector to hyprland

---
+++ dotfiles/.config/waybar/scripts/select-wallpaper.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+function get_wallpapers {
+  local wallpapers wallpaper
+  
+  wallpapers=$(find "$HOME/Pictures/Wallpapers" -type f \( -name "*.jpg" -o -name "*.png" \))
+
+  for wallpaper in $wallpapers; do
+    printf "%s\x00icon\x1f%s\n" "$(basename "$wallpaper")" "$wallpaper"
+  done
+}
+
+CHOICE=$(get_wallpapers | rofi -dmenu -i -p "Wallpapers" -theme ~/.config/rofi/config.rasi)
+swaybg -i "$CHOICE" -m fill
